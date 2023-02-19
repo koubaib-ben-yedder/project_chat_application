854df6a15ea373e9d55273a1d7167d99f488a23f
Your Name <you@example.com>
2023-02-20 00:11:04 +1100

    fix jenkins problem

--- client/script.sh
+++ client/script.sh
@@ -1,4 +1,4 @@
-sh "sudo useradd frontend"
-sh "sudo groupadd docker"
-sh "sudo usermod -aG docker frontend"
+
+
 sh "sudo newgrp docker"
+sh "sudo gsroupadd docker"
--- server/script.sh
+++ server/script.sh
@@ -1,4 +1,6 @@
-sh "sudo useradd frontend"
-sh "sudo groupadd docker"
-sh "sudo usermod -aG docker frontend"
+
+
+
+
 sh "sudo newgrp docker"
+sh "sudo groupadd docker"
